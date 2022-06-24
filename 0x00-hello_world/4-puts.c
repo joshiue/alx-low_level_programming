2da5c5fa88d147448dc31fe17b0534092ed2038b
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-06-24 08:58:48 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - print a message in c
+ *
+ * Return: none
+ */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
 }
