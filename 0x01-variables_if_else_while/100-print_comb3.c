c7dd39a15059471a4fce012e5d0712fd734dd3dc
Joshua <jaitonit@yahoo.co.uk>
2022-07-01 23:30:23 -0700

    update task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - root function
- *
- * Return: always 0
- **/
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int t;
-	int o;
+int ch;
+int n;
+for (ch = 48; ch <= 57; ch++)
+{
+for (n = 49; n <= 57; n++)
+{
+if (n > ch)
+{
+putchar(ch);
+putchar(n);
+if (ch != 56 || n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
 
-	for (t = 0; t <= 9; t++)
-	{
-		for (o = 0; o <= 9; o++)
-		{
-			if (o > t)
-			{
-				if (!(t == 0 && o == 1))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+putchar(10); /* this is an ascii code for new line*/
 
-				putchar(t + '0');
-				putchar(o + '0');
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+return (0);
 }
