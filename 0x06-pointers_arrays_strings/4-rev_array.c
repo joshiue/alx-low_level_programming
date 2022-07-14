6fb41b6158e3c3ab7f034d0ae65ce5361db0ec7d
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-14 16:09:27 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,43 +1,22 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- * * reverse_array -> reversing an array
- *
- * * @a: array a
- *
- * * @n: an element of an array
- *
- * */
-
+* reverse_array -> reversing an array
+* @a: array a
+* @n: an element of an array
+*/
 void reverse_array(int *a, int n)
-
 {
+	int *p, i, aux, k;
 
-		int *p, i, aux, k;
-
-
-
-			p = a;
-
-				for (i = 0; i < n; i++)
-
-							p++;
-
-					for (k = 0; k < i / 2; k++)
-
-							{
-
-										aux = a[k];
-
-												a[k] = *p;
-
-														*p = aux;
-
-																p--;
-
-																	}
-
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
 }
