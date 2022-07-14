5abb78939561232e452e54a65e7987aa522f9bdf
Joshua <jaitonit@yahoo.co.uk>
2022-07-14 11:30:45 -0700

    Advanced tasks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,19 @@
-#include "main.h"
+nclude "main.h"
+
+
 
 /**
- * reset_to_98 -> a function to update the reference of a pointer
- * @n: pointer to n to be updated
- */
+ *
+ * * reset_to_98 -> a function to update the reference of a pointer
+ *
+ * * @n: pointer to n to be updated
+ *
+ * */
+
 void reset_to_98(int *n)
+
 {
-	*n = 98;
+
+		*n = 98;
+
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,29 @@
-#include "main.h"
+nclude "main.h"
+
+
 
 /**
-* swap_int -> given two integers swap the values they are holding
-* @a: parameter 1
-* @b: parameter 2
-*/
-void swap_int(int *a, int *b)
-{
-	int tmp;
+ *
+ * * swap_int -> given two integers swap the values they are holding
+ *
+ * * @a: parameter 1
+ *
+ * * @b: parameter 2
+ *
+ * */
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+void swap_int(int *a, int *b)
+
+{
+
+		int tmp;
+
+
+
+			tmp = *a;
+
+				*a = *b;
+
+					*b = tmp;
+
 }
