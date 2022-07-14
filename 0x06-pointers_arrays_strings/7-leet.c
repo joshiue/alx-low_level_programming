825b43a0652927f66fd7edc5087a4b58fe03a0e0
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-14 16:12:01 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,53 +1,27 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- * * leet -> a leet function
- *
- * * @x: param x
- *
- * * Return: a string
- *
- * */
-
+* leet -> a leet function
+* @x: param x
+* Return: a string
+*/
 char *leet(char *x)
-
 {
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
 
-		int a = 0, b, l = 5;
+	while (x[a])
+	{
+		b = 0;
 
-			char tr[5] = {'A', 'E', 'O', 'T', 'L'};
-
-				char trw[5] = {'4', '3', '0', '7', '1'};
-
-
-
-					while (x[a])
-
-							{
-
-										b = 0;
-
-
-
-												while (b < l)
-
-															{
-
-																			if (x[a] == tr[b] || x[a] - 32 == tr[b])
-
-																								x[a] = trw[b];
-
-																						b++;
-
-																								}
-
-														a++;
-
-															}
-
-						return (x);
-
+		while (b < l)
+		{
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+				x[a] = trw[b];
+			b++;
+		}
+		a++;
+	}
+	return (x);
 }
