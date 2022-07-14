49047a843b09fc3b39c04deb897315b4879abdd4
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-14 19:45:41 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,69 +1,29 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
-int main(void) 
-
+/**
+ * main - generates keygen
+ * Return: 0
+ */
+int main(void)
 {
-
-		srand(time(NULL));
-
-			int x;
-
-				int cifras = 8;
-
-					char min[] = "abcdefghijklmnopqrstuvwxyz";
-
-						char may[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-							char num[] = "0123456789";
-
-								char aleatorio[cifras+1];
-
-
-
-									for (x = 0; x < cifras; x++) 
-
-											{
-
-														int eleccion = (int)(rand() % 3 + 1);
-
-
-
-																switch ( eleccion ) 
-
-																			{
-
-																							case 1:
-
-																												aleatorio[x] = min[rand() % 25 + 1];
-
-																																break;
-
-																																			case 2:
-
-																																				aleatorio[x] = may[rand() % 25 + 1];
-
-																																								break;
-
-																																											case 3:
-
-																																												aleatorio[x] = num[rand() % 9 + 1];
-
-																																																break;
-
-																																																		}
-
-																		aleatorio[cifras] = '\0';
-
-																			}
-
-										printf("%s\n", aleatorio);
-
-											return (0);
-
+int pass[100];
+int i, sum, n;
+sum = 0;
+srand(time(NULL));
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
+return (0);
 }
