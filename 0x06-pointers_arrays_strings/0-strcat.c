0755d973a3e859b7a1aeee4dd3732bb7805b7bb1
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-14 16:06:43 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,47 +1,24 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 
 
 /**
- *
- * * _strcat -> this is a funcion strcat
- *
- * * @dest: first param
- *
- * * @src: second param
- *
- * * Return: a string
- *
- * */
-
+* _strcat -> this is a funcion strcat
+* @dest: first param
+* @src: second param
+* Return: a string
+*/
 char *_strcat(char *dest, char *src)
-
 {
+	int len = 0, i;
 
-		int len = 0, i;
+	while (dest[len])
+		len++;
 
-
-
-			while (dest[len])
-
-						len++;
-
-
-
-				for (i = 0; src[i] != 0; i++)
-
-						{
-
-									dest[len] = src[i];
-
-											len += 1;
-
-												}
-
-					dest[len] = '\0';
-
-						return (dest);
-
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
 }
