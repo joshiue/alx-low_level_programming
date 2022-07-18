a766a43dbd4abea8b6a51f0f56e45eaa5cd975ac
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-18 14:17:33 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
-		*(dest + i) =  *(src + i);
-
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	return (dest);
+
+
+
 }
