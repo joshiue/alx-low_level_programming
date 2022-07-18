cbe910c8d9b18adf09e41a15411716ab6fc86a34
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-18 13:05:25 +0100

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,16 +8,14 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
-	/*Delacring FOR*/
-	for (i = 0; i < n; i++)
+	i = 0;
+	while (i < n)
 	{
-		*(s + i) = b; /*add 1 position s*/
-
-	} /*END FOR*/
-
+		s[i] = b;
+		i++;
+	}
 	return (s);
 }
