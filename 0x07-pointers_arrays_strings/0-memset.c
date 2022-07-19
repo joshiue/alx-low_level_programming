3e5c5cf46253d8b9570f153ba66be514351b607d
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-19 04:54:41 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,15 @@
 #include "main.h"
+/**print_alphabet - function to *print abc
+*
+* Return: 0
+*/
+void print_alphabet(void)
+{ 
+char c;
 
-/**
- * _memset - ironnou
- * @s: pointer to char params
- * @b: data to change
- * @n: index
- * Return: *s
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-        unsigned int i;
-
-        for (i = 0; i < n; i++)
-        {
-		*(s + i) = b;
-        }
-        return (s);
+for (c = 'a'; c <= 'z; c++
+}
+_putchar(c); 
+} 
+_putchar ('\n');
 }
