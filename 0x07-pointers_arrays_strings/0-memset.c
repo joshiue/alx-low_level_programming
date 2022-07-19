70cc41456b452008fbdcbece08ca3a54d4ccc0a9
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-19 04:55:29 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,17 @@
 #include "main.h"
-/**print_alphabet - function to *print abc
-*
-* Return: 0
-*/
-void print_alphabet(void)
-{ 
-char c;
+/**
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
-for (c = 'a'; c <= 'z; c++
-}
-_putchar(c); 
-} 
-_putchar ('\n');
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
 }
