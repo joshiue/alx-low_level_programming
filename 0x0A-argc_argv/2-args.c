dafc988079e01e58dda959f15666e87f8e8d4a6c
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-22 23:36:46 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,35 +1,15 @@
-nclude <stdio.h>
-
-
-
+#include <stdio.h>
 /**
-
-  * main -> this is a function to print its name
-
-  * @argc: argc parameter
-
-  * @argv: an array of a command listed
-
-  * Return: 0 for success
-
-  */
-
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
 int main(int argc, char *argv[])
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 0; i < argc; i++)
-
-					{
-
-								printf("%s\n", argv[i]);
-
-									}
-
-				return (0);
-
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
 }
