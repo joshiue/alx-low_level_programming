1022edc4ee3d746517ce6059a208c6c5fa845a27
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-25 07:26:45 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _strdup - Creates a pointer with a copy of the str param
- *
- * @str: String to copy
- *
- * Return: a pointer to the duplicated string or
- * null if the memory is insufficient or if the str is NULL
- */
-
+* _strdup - Creates a pointer with a copy of the str param
+*
+* @str: String to copy
+*
+* Return: a pointer to the duplicated string or
+* null if the memory is insufficient or if the str is NULL
+*/
 char *_strdup(char *str)
 {
 	char *duplicated_str;
@@ -16,20 +15,15 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-
 	while (str[size])
 		size++;
-
 	duplicated_str = malloc((size + 1) * sizeof(char));
-
 	if (duplicated_str == NULL)
 		return (NULL);
-
 	while (i < size)
 	{
 		duplicated_str[i] = str[i];
 		i++;
 	}
-
 	return (duplicated_str);
 }
