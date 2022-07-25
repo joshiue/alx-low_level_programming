c9d89429bafd6b5bef1ad1161d1d7a121a6b7d20
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-25 07:02:09 +0100

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* main - Prints the addition of positive numbers,
-*followed by a new line.
-* @argc: The number of arguments passed to the program.
-* @argv: An array of pointers to the arguments.
-* Return: If one of the numbers contains symbols that are non-digits - 1.
-*Otherwise - 0.
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
 */
 int main(int argc, char *argv[])
 {
-int num, digit, sum = 0;
-for (num = 1; num < argc; num++)
-{
-for (digit = 0; argv[num][digit]; digit++)
-{
-if (argv[num][digit] < '0' || argv[num][digit] > '9')
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[num]);
-}
-printf("%d\n", sum);
-return (0);
+	int result = 0, num, i, j, k;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
 }
