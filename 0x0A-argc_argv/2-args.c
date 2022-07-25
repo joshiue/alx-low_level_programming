9198f31a63b7432ccbb0ea2b10e546edf0d52317
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-25 06:58:27 +0100

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+
 /**
-* main -  prints all arguements
-* @argc: arguement count
-* @argv: array of pointers to CLI arguement
-* Return: 0
-**/
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
