6e37163fe3e2d1385c3da4ea3be349f1fbef0daa
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-08-23 05:36:10 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
-#ifndef _MAIN_
-#define _MAIN_
+
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
